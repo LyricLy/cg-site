6cb27bf86f594665fee40858d693794871d6d10c
Christina Hanson <christinahansondesu@gmail.com>
2022-03-30 07:57:26 +1300

    Update lanuages

--- cg.py
+++ cg.py
@@ -171,7 +171,7 @@ def rank_enumerate(xs, *, key):
             cur_key = key(x)
         yield (cur_idx, x)
 
-LANGUAGES = ["py", "rs", "bf", "hs", "c", "png", "text"]
+LANGUAGES = ["py", "rs", "bf", "hs", "c", "go", "zig", "d", "png", "text"]
 META = """
 <link rel="icon" type="image/png" href="/favicon.png">
 <meta charset="utf-8">
