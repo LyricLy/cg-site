0b23893714e317606f39f7db7a6e406fbf7e2067
LyricLy <christinahansondesu@gmail.com>
2022-10-24 10:08:21 +1300

    Uncomment reshuffl3

--- reshuffle.py
+++ reshuffle.py
@@ -7,8 +7,7 @@ db = sqlite3.connect("the.db")
 db.row_factory = sqlite3.Row
 num, = db.execute("SELECT MIN(num) FROM Rounds WHERE stage = 2")
 subs = db.execute("SELECT * FROM Submissions WHERE round_num = ? ORDER BY position", num).fetchall()
-print([x["position"] for x in subs])
-#db.execute("UPDATE Submissions SET position = NULL WHERE round_num = ?", num)
-#for idx, sub in enumerate(subs, start=1):
-#    db.execute("UPDATE Submissions SET position = ? WHERE round_num = ? AND author_id = ?", (idx, sub["round_num"], sub["author_id"]))
-#db.commit()
+db.execute("UPDATE Submissions SET position = NULL WHERE round_num = ?", num)
+for idx, sub in enumerate(subs, start=1):
+    db.execute("UPDATE Submissions SET position = ? WHERE round_num = ? AND author_id = ?", (idx, sub["round_num"], sub["author_id"]))
+db.commit()
