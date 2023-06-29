ce74b9b99c02cc698966b608b6af79b395a8b263
Christina Hanson <christinahansondesu@gmail.com>
2023-06-29 20:46:05 +1200

    Hide Likes and Comments

--- end.py
+++ end.py
@@ -10,5 +10,9 @@ num, = db.execute("UPDATE Rounds SET stage = 3, ended_at = ? WHERE stage = 2 RET
 for persona in db.execute("SELECT persona FROM Submissions WHERE round_num = ?", (num,)):
     db.execute("UPDATE Comments SET persona = -1, og_persona = IIF(og_persona IS NULL, persona, og_persona) WHERE persona = ?", persona)
 db.commit()
-shutil.copy("the.db", "static/once.db")
+shutil.copy("the.db", "once.db")
+shutil.copy("the.db", "/static/the.db")
+public_db = sqlite3.connect("/static/the.db")
+public_db.execute("DROP TABLE Likes")
+public_db.execute("DROP TABLE Comments")
 requests.post(config.canon_url + "/personas/purge")
