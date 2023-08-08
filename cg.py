475d2d2fd261e0b90d98a3f7780890b230ab0c2d
Christina Hanson <christinahansondesu@gmail.com>
2023-08-08 20:01:43 +1200

    Remove surviving references to hl_content

--- cg.py
+++ cg.py
@@ -677,7 +677,7 @@ def take(num):
                         value = None 
                     if value not in LANGUAGES:
                         flask.abort(400)
-                    db.execute("UPDATE Files SET hl_content = NULL, lang = ? WHERE round_num = ? AND name = ?", (value, num, key))
+                    db.execute("UPDATE Files SET lang = ? WHERE round_num = ? AND name = ?", (value, num, key))
             case ("guess", 2):
                 db.execute("DELETE FROM Guesses WHERE round_num = ? AND player_id = ?", (num, user.id))
                 guesses = form.getlist("guess")
--- modify_for_public_viewing.sql
+++ modify_for_public_viewing.sql
@@ -1,6 +1,6 @@
 DROP TABLE Likes;
 DROP TABLE Comments;
-ALTER TABLE Files DROP COLUMN hl_content;
+ALTER TABLE Submissions DROP COLUMN cached_display;
 ALTER TABLE Submissions DROP COLUMN persona;
 ALTER TABLE Submissions DROP COLUMN finished_guessing;
 ALTER TABLE Guesses DROP COLUMN locked;
