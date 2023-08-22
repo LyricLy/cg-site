27583359fd77294d987843d96d0e5a1a92ba9757
Christina Hanson <christinahansondesu@gmail.com>
2023-08-23 01:01:50 +1200

    Add factor, forth and elm

--- cg.py
+++ cg.py
@@ -415,7 +415,8 @@ def rank_enumerate(xs, *, key):
 LANGUAGES = [
     "py", "c", "rs", "js", "ts", "bf", "hs", "lua", "zig", "cpp", "go", "java", "kotlin", "groovy",
     "d", "swift", "pl", "scm", "raku", "apl", "sml", "ocaml", "f#", "erlang", "dart", "pony", "ada",
-    "vim", "sed", "nix", "sh", "xml", "yaml", "json", "befunge", "image", "text", None
+    "forth", "factor", "elm",
+    "vim", "sed", "nix", "sh", "html", "xml", "yaml", "json", "befunge", "image", "text", None
 ]
 META = """
 <link rel="icon" type="image/png" href="/favicon.png">
