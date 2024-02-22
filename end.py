12d66dd35677f6da390f59cf629c3c0cc5a2ad25
Christina Hanson <christinahansondesu@gmail.com>
2024-02-23 12:52:28 +1300

    Break ties automatically

--- end.py
+++ end.py
@@ -1,5 +1,6 @@
 import sqlite3
 import datetime
+import hashlib
 import os
 import shutil
 import requests
@@ -10,6 +11,14 @@ db = sqlite3.connect("the.db")
 num, = db.execute("UPDATE Rounds SET stage = 3, ended_at = ? WHERE stage = 2 RETURNING num", (datetime.datetime.now(datetime.timezone.utc),)).fetchone()
 for persona in db.execute("SELECT persona FROM Submissions WHERE round_num = ?", (num,)):
     db.execute("UPDATE Comments SET persona = -1, og_persona = COALESCE(og_persona, persona) WHERE persona = ?", persona)
+
+# tiebreak
+winners = db.execute("SELECT player_id FROM Scores WHERE round_num = ? AND won ORDER BY player_id", (num,)).fetchall()
+winner_idx = int.from_bytes(hashlib.sha256(str(num).encode()).digest(), "big") % len(winners)
+for idx, so_called_winner in enumerate(winners):
+    if idx != winner_idx:
+        db.execute("INSERT INTO Tiebreaks (round_num, player_id, new_rank) VALUES (?, ?, ?)", (num, so_called_winner, 2))
+
 db.commit()
 os.makedirs("backups", exist_ok=True)
 shutil.copy("the.db", f"backups/{num}.db")
