96bd72530088d9d82c12a2a6de3a21f483fc04b6
Christina Hanson <christinahansondesu@gmail.com>
2024-03-18 12:19:09 +1300

    Make start.py clear existing rounds

--- start.py
+++ start.py
@@ -8,5 +8,6 @@ with open(sys.argv[1]) as f:
     spec = f.read()
 
 now = datetime.datetime.now(datetime.timezone.utc)
+db.execute("DELETE FROM Rounds WHERE stage = 1")
 db.execute("INSERT INTO Rounds (stage, spec, started_at, stage2_at) VALUES (1, ?, ?, ?)", (spec, now, now+datetime.timedelta(days=7)))
 db.commit()
